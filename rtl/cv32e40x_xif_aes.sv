85565793f90f69d4b07b8247c30060e6bc1119d9
Aleksander Waage <aleksander.waage@ubiqaerospace.com>
2022-02-15 18:45:33 +0100

    Almost completed the extension interface for AES FU. Missing the commit interface. Ready for synthesis fixes

--- rtl/cv32e40x_xif_aes.sv
+++ rtl/cv32e40x_xif_aes.sv
@@ -25,6 +25,7 @@ module cv32e40x_xif_aes import cv32e40x_pkg::*;
     logic encrypt_middle_i, encrypt_i, decrypt_i, decrypt_middle_i;
     logic valid_aes_result;
 
+<<<<<<< HEAD
     logic [1:0]              byte_select_i;
     logic [X_RFR_WIDTH-1:0]  rs1_i, rs2_i, result_aes_o, rd;
     logic [X_ID_WIDTH-1:0]   instruction_id;
@@ -126,6 +127,81 @@ module cv32e40x_xif_aes import cv32e40x_pkg::*;
             if(instruction_id == xif_commit.commit.id) begin
                 is_instruction_not_kill = 0;
             end
+=======
+    logic valid_i, ready_i;
+    logic encrypt_middle_i, encrypt_i, decrypt_i, decrypt_middle_i;
+    logic [X_RFR_WIDTH-1:0] rs1_i, rs2_i, rd_i;
+    logic [1:0] byte_select_i;
+    logic [X_ID_WIDTH-1:0] id_in_aes_fu;
+
+
+
+    // Accept offloaded instruction logic
+    always_comb
+    begin
+        unique case(xif_issue.issue_req.instr[6:0])
+            AES32: begin
+                // accept instruction
+                xif_issue.issue_resp.accept = 1;
+                xif_issue.issue_resp.writeback = 1;
+                xif_issue.issue_resp.dualwrite = 0;
+                xif_issue.issue_resp.loadstore = 0;
+                xif_issue.issue_resp.ecswrite = 0;
+                xif_issue.issue_resp.exc = 0; //? what is a synchronous exception
+            end
+            default: begin
+                //Dont accept instruction
+                xif_issue.issue_resp.accept = 0;
+                xif_issue.issue_resp.writeback = 0;
+                xif_issue.issue_resp.dualwrite = 0;
+                xif_issue.issue_resp.loadstore = 0;
+                xif_issue.issue_resp.ecswrite = 0;
+                xif_issue.issue_resp.exc = 0; //? what is a synchronous exception
+            end
+        endcase
+    end
+
+    always_ff @(posedge clk, negedge rst_n)
+        begin : ID_FU_AES
+        if (rst_n == 1'b0)
+        begin
+            rs1_i = '0;
+            rs2_i = '0;
+        end else 
+        begin
+            rs1_i            = '0;
+            rs2_i            = '0;
+            valid_i          = 1;
+            decrypt_i        = 0;
+            decrypt_middle_i = 0;
+            encrypt_i        = 0;
+            encrypt_middle_i = 0;
+
+            id_in_aes_fu = xif_issue.issue_req.id;
+            if(xif_issue.issue_req.rs_valid == 3'b??1)begin
+                rs1_i = xif_issue.issue_req.rs[0];
+            end else
+            begin
+                valid = 0;
+            end
+
+            if(xif_issue.issue_req.rs_valid == 3'b?1?)begin
+                rs2_i = xif_issue.issue_req.rs[1];
+            end else
+            begin
+                valid_i = 0;
+            end
+
+            byte_select = xif_issue.issue_req.instr[31:30];
+            unique case(xif_issue.issue_req.instr[29:25])
+                AES32DSI:  decrypt_i        = 1;
+                AES32DSMI: decrypt_middle_i = 1;
+                AES32ESI:  encrypt_i        = 1;
+                AES32ESMI: encrypt_middle_i = 1;
+            endcase
+            rd_register_adr = xif_issue.issue_req.instr[11:7];
+
+>>>>>>> 5c29bfe (Almost completed the extension interface for AES FU. Missing the commit interface. Ready for synthesis fixes)
         end
     end
 
@@ -134,6 +210,7 @@ module cv32e40x_xif_aes import cv32e40x_pkg::*;
     begin : AES_FU_RESULTS
         if (rst_n == 1'b0)
         begin
+<<<<<<< HEAD
             valid_aes_result =  0;
             rd           = '0;
         end else 
@@ -144,18 +221,41 @@ module cv32e40x_xif_aes import cv32e40x_pkg::*;
                 rd = result_aes_o;
                 valid_aes_result = 1;
             end
+=======
+            
+        end else 
+        begin
+            if(xif_result.result_valid == 1 && xif_result.result_ready == 1)
+                xif_result.result_valid = 0;
+
+            if(ready_o) begin
+                xif_result.result.id = id_in_aes_fu;
+                xif_result.result.data      = rd_o;
+                xif_result.result.rd        = rd_register_adr;
+                xif_result.result.we        = 0;
+                xif_result.result_valid     = 1;
+            end
+            
+>>>>>>> 5c29bfe (Almost completed the extension interface for AES FU. Missing the commit interface. Ready for synthesis fixes)
         end
     end
 
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 5c29bfe (Almost completed the extension interface for AES FU. Missing the commit interface. Ready for synthesis fixes)
     riscv_crypto_fu_saes32 
     #(
         .SAES_DEC_EN ( 1 )                 // Enable saes32 decrypt instructions.
     )
     aes_i
     (
+<<<<<<< HEAD
         .valid           (valid_aes_input)  , // Are the inputs valid?
+=======
+        .valid           (valid_i)          , // Are the inputs valid?
+>>>>>>> 5c29bfe (Almost completed the extension interface for AES FU. Missing the commit interface. Ready for synthesis fixes)
         .rs1             (rs1_i)            , //[31:0] //! (modified) Key   
         .rs2             (rs2_i)            , //[31:0]//! Cipher 
         .bs              (byte_select_i)    , //[1:0]// Byte select immediate
@@ -165,8 +265,13 @@ module cv32e40x_xif_aes import cv32e40x_pkg::*;
         .op_saes32_decs  (decrypt_i)        , // Decrypt SubBytes
         .op_saes32_decsm (decrypt_middle_i) , // Decrypt SubBytes + MixColumn
 
+<<<<<<< HEAD
         .rd              (result_aes_o)     , //[31:0]// output destination register value.
         .ready           (ready_aes_output)   // Compute finished?
+=======
+        .rd              (rd_o)             , //[31:0]// output destination register value.
+        .ready           (ready_o)            // Compute finished?
+>>>>>>> 5c29bfe (Almost completed the extension interface for AES FU. Missing the commit interface. Ready for synthesis fixes)
     );
 
 
